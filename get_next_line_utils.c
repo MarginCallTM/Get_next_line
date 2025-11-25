9cce82fc763614bea88e5af0247cb078a2f9b782
Adrien Combier <acombier@z4r10p3.42lyon.fr>
2025-11-25 12:44:55 +0100

    Creation of utils function

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,36 @@
 /*   By: acombier <acombier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/25 11:21:26 by acombier          #+#    #+#             */
-/*   Updated: 2025/11/25 11:21:27 by acombier         ###   ########.fr       */
+/*   Updated: 2025/11/25 12:44:14 by acombier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+
+size_t	ft_strlen(const char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while(str[i])
+	{
+		i++;
+	}
+	return (i);
+}
+
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	
+}
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	
+}
