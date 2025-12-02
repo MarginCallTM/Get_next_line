441736a75a7789b664a4ffad6d3c851e2a8389d9
Adrien Combier <acombier@z4r11p2.42lyon.fr>
2025-12-02 10:30:44 +0100

    fix the strjoin

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -3,82 +3,80 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line_utils.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: adriencombier <adriencombier@student.42    +#+  +:+       +#+        */
+/*   By: acombier <acombier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/25 11:21:26 by acombier          #+#    #+#             */
-/*   Updated: 2025/11/28 12:39:19 by adriencombi      ###   ########.fr       */
+/*   Updated: 2025/12/02 10:30:13 by acombier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+//#include "get_next_line.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 size_t	ft_strlen(const char *str)
 {
 	size_t	i;
 
 	i = 0;
-	while(str[i])
+	while (str[i])
 	{
 		i++;
 	}
 	return (i);
 }
 
-
-char	*ft_strjoin(char *left_str, char *buff)
+char	*ft_strjoin(char *stash, char *buff)
 {
 	size_t	i;
 	size_t	j;
-	char	*str;
-
-	if (!left_str)
+	size_t	total_len;
+	char	*joined;
+	
+	i = 0;
+	if(!stash || !buff)
+		return (NULL);
+	total_len = ft_strlen(stash) + ft_strlen(buff);
+	joined = malloc(total_len + 1);
+	if(!joined)
+		return (NULL);
+	while(stash[i])
 	{
-		left_str = (char *)malloc(1 * sizeof(char));
-		left_str[0] = '\0';
+		joined[i] = stash[i];
+		i++;
 	}
-	if (!left_str || !buff)
-		return (NULL);
-	str = malloc(sizeof(char) * ((ft_strlen(left_str) + ft_strlen(buff)) + 1));
-	if (str == NULL)
-		return (NULL);
-	i = -1;
 	j = 0;
-	if (left_str)
-		while (left_str[++i] != '\0')
-			str[i] = left_str[i];
-	while (buff[j] != '\0')
-		str[i++] = buff[j++];
-	str[ft_strlen(left_str) + ft_strlen(buff)] = '\0';
-	free(left_str);
-	return (str);
+	while(buff[j])
+	{
+		joined[i + j] = buff[j];
+		j++;
+	}
+	joined[i + j] = '\0';
+	return(joined);
 }
-
 
 char	*ft_strchr(const char *s, int c)
 {
 	size_t	i;
 
 	i = 0;
-	while(s[i])
+	while (s[i])
 	{
-		if(s[i] == (char)c)
-			return((char *)(s + i));
+		if (s[i] == (char)c)
+			return ((char *)(s));
 		i++;
 	}
-	if((char)c == '\0')
-		return((char *)(s + i));
+	if ((char)c == '\0')
+		return ((char *)(s));
 	return (NULL);
 }
-
 
 char	*ft_strdup(const char *s)
 {
 	char	*dup;
 	size_t	i;
-	
-	i = 0;
 
+	i = 0;
 	dup = malloc(ft_strlen(s) + 1);
 	if (!dup)
 		return (NULL);
@@ -90,8 +88,6 @@ char	*ft_strdup(const char *s)
 	dup[i] = '\0';
 	return (dup);
 }
-
-
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
@@ -115,26 +111,26 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	return (sub);
 }
 
-
-
-int main(void)
+int	main(void)
 {
-	printf("%zu", ft_strlen("Hello Earth!"));
-	char	s1[] = "\nouioui ";
-	char	s2[] = "Baguette\n";
-
-	int	c = 'H';
-	char	s3[] = "Petit Honey\n";
-
-	char	s4[] = "Les saucisses sont cuites!\n";
-	size_t len = 13;
-
-	ft_strjoin(s1, s2);
-	ft_strchr(s3, c);
+	//printf("%zu", ft_strlen("Hello Earth!"));
+	char s1[] = "ouioui ";
+	char s2[] = "Baguette\n";
 
 	printf("%s", ft_strjoin(s1, s2));
-	printf("%s", ft_strchr(s3, c));
-	printf("%s", ft_substr(s4, 0, 12));
 
-	return(0);
+	// int c = '\n';
+	// char s3[] = "Petit Honey\n";
+
+	// char s4[] = "Les saucisses sont cuites!\n";
+	// size_t len = 13;
+
+	// ft_strjoin(s1, s2);
+	//ft_strchr(s3, c);
+
+	// printf("%s", ft_strjoin(s1, s2));
+	// printf("%s", ft_strchr(s3, c));
+	// printf("%s", ft_substr(s4, 0, 12));
+
+	return (0);
 }
